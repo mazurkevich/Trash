42916f1c4023f1fcab1ce5305312f5cb9518e7ed
avnasedkin <kloudsnuff@gmail.com>
2014-07-04 13:07:04 +0400

    Project

---
+++ PMSOFT_Practics/Twitter_Killer/ViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Input;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using RestSharp;
+
+namespace Twitter_Killer
+{
+    class ViewModel : ViewModelBase
+    {
+        private string _login;
+        private string _password;
+        private IUser _user;
+
+        public ICommand LoginCommand { get; private set; }
+        
+        public string Login
+        {
+            get { return _login; }
+            set { Set(ref _login, value); }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { Set(ref _password, value); }
+        }
+
+        public ViewModel()
+        {
+            LoginCommand = new RelayCommand(OnLogin);
+        }
+         
+        private void OnLogin()
+        {
+           
+        }
+    }
+}
