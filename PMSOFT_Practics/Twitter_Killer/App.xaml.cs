3c3e3135e5f824a7642b9cf2dbc53edf57ea6931
avnasedkin <kloudsnuff@gmail.com>
2014-07-04 11:30:24 +0400

    project

---
+++ PMSOFT_Practics/Twitter_Killer/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace Twitter_Killer
+{
+    public partial class App
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // initializing client view with data context
+            var clientView = new View {DataContext = new ViewModel()};
+            clientView.Show();
+        }
+    }
+}
